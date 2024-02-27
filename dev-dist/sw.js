b14981b77837be7347fa06d085aaf701bf7fe17f
brunaanjos <brunaanjosdaveiga@gmail.com>
2024-02-27 10:45:59 -0300

    testanto aplicação 27/02/24

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a06e8uu7c7g"
+    "revision": "0.7if6pm3bmko"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
