bf2b0d6050f01881a4fadcfda0f7bfa1bc5858bf
brunaanjos <brunaanjosdaveiga@gmail.com>
2024-03-04 11:14:01 -0300

    primeira aula de composables 04/03/24

--- src/components/ListagemProdutos.vue
+++ src/components/ListagemProdutos.vue
@@ -2,7 +2,9 @@
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
 const produtos = ref([]);
+import { useScreen } from '@/composables/screen';
 
+const { browserWidth, deviceWidth, isMobile } = useScreen();
 onMounted(async () => {
   const response = await axios.get('https://fakestoreapi.com/products');
   produtos.value = response.data;
@@ -16,7 +18,9 @@ const formatPrice = (price) => `R$ ${price.toFixed(2).replace('.', ',')}`;
     <h1>Produtos</h1>
     <div class="container">
       <div class="card" v-for="produto in produtos" :key="produto.id">
-        <h1 class="card--title">{{ produto.title }}</h1>
+        <h1>Produtos - {{ browserWidth }} - {{ deviceWidth }} - {{
+      isMobile}} 
+      <span v-if="isMobile">É móvel</span></h1>
         <p>{{ produto.description }}</p>
         <p>{{ formatPrice(produto.price) }}</p>
         <img class="card--avatar" :src="produto.image" :alt="produto.title" />
